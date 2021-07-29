08ff3a7ec5f3d14628347eaee6658b7ba56eeb6d
Andira Yunita <andhirayp@gmail.com>
2021-07-29 23:09:54 +0700

    adding enums desc and syntax

--- CollectionTypes.swift
+++ CollectionTypes.swift
@@ -50,4 +50,62 @@ var result = Array<Int>()
 
 // ------------------------------------------------
 
-// enums
+let opening3 = "\n=====Enumerations====="
+print(opening3)
+
+// enums: a way of defining groups of related values in a way that makes them easier to use.
+
+let result1 = "failure"
+let result2 = "failed"
+let result3 = "fail"
+
+enum Result {
+    case success
+    case failure
+}
+let result4 = Result.failure
+let result5 = Result.success
+print("The result is: \(result4)")
+print("Another result is: \(result5)")
+
+enum Direction {
+    case north
+    case south
+    case west
+    case east
+}
+let whichDirection = Direction.north
+print("The direction is in the \(whichDirection).")
+
+
+// enums associated values
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+let talking = Activity.talking(topic: "football")
+print(talking)
+
+
+// enums raw values
+
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+let earth = Planet.earth.rawValue
+print("The value of earth is \(earth).")
+
+enum Planet2: Int {
+    case mercury2 = 1
+    case venus2 
+    case earth2
+    case mars2
+}
+let earthOrder = Planet2.earth2.rawValue
+print("Earth has value \(earthOrder).")
