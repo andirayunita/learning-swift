82c5fd87653734c4c411e3e8e47b7f6793b74eb8
Andira Yunita <andhirayp@gmail.com>
2021-08-02 05:38:35 +0700

    variadic throws functions

--- HWS/Functions.swift
+++ HWS/Functions.swift
@@ -92,3 +92,93 @@ func greet(_ person: String) {
 }
 greet("Harry")
 
+//--------------------------------------------------------------------------------------------
+
+// default parameters
+// write an = after the parameter's type followed by the default value
+print("\n====Default Parameters====")
+
+func greeting(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")        
+    } else {
+        print("Oh no.. It's \(person) again.")
+    }
+}
+greeting("Andira")
+greeting("Andira", nicely: false)
+
+
+func findDirections(from: String, to: String, route: String = "fastest", avoidHighways: Bool = false) {
+    print("The direction \(from)-\(to) is \(route), and the route to avoid the highways is \(avoidHighways)")
+}
+findDirections(from: "Jakarta", to: "Bekasi")
+findDirections(from: "Jakarta", to: "Bandung", route: "scenic")
+findDirections(from: "Jakarta", to: "Bogor", route: "scenic", avoidHighways: true)
+
+//--------------------------------------------------------------------------------------------
+
+// variadic functions
+// write ... after parameter's data types,
+// for example: Int... means zero or more integers, potientally hundreds
+print("\n====Variadic Functions====")
+
+func squares(someNumbers: Int...) {
+    for number in someNumbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+squares(someNumbers: 1, 2, 3, 4, 5)
+
+//--------------------------------------------------------------------------------------------
+
+// writing throwing functions
+// define an enum that desc error we can throw
+// write throws keyword, before their return type,
+// then using throw keyword when something goes wrong
+print("\n====Throwing Functions====")
+
+enum BookErrors: Error {
+    case tooFewPages
+    case tooManyPages
+}
+func writeBook(title: String, page: Int) throws {
+    switch page {
+        case 0...50:
+            throw BookErrors.tooFewPages
+        case 51...400:
+            print("Perfect! I'm going to write \(title)...")
+        default:
+            throw BookErrors.tooManyPages
+    }
+}
+
+
+enum PasswordError: Error {
+    case obvious
+}
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    return true
+}
+
+// running throwing functions: three unique keyword such as do, try, catch
+// do keyword starts a section of code that might cause problems
+// try keyword is used before every function that might throw an error
+// catch keyword lets handle errors
+do {
+    try checkPassword("password")
+    print("Your password is good.")
+} catch {
+    print("You can't use that passowrd.")
+}
+
+
+//--------------------------------------------------------------------------------------------
+
+// inout parameters
+// 
+print("\n====inout Parameters====")
+
