6c53ec0d25487e0cb26ae075c4adc3337b5b0876
Andira Yunita <andhirayp@gmail.com>
2021-08-02 10:01:27 +0700

    create closures, accept parameters, returning value closure

--- HWS/Closures.swift
+++ HWS/Closures.swift
@@ -3,16 +3,47 @@ print("\t\t\t\t  Closures")
 print("============================================")
 
 // creating basic closures
-// print("\n====Creating Basic Closures====")
+// closures are like functions but written differently
+// 
+print("\n====Creating Basic Closures====")
+
+let driving = {
+    print("I'm driving in my car")
+}
+driving()
+
+//--------------------------------------------------------------------------------------------
 
 // accepting parameters in a closure
-// print("\n====Accepting Parameters in a Closure====")
+// after the opening braces, list the parameters inside the parentheses,
+// then write _in_ keyword after parameters
+print("\n====Accepting Parameters in a Closure====")
+
+let driving1 = { (place: String) in
+    print("I'm going to \(place) in my car!")
+}
+driving1("Jakarta")    //don't use parameters labels when running closures
+
+//--------------------------------------------------------------------------------------------
 
 // returning value from a closure
-// print("\n====Returning Value From a Closure====")
+// write the return value data types before the in keyword,
+// then use the return keyword inside the closure like a function
+print("\n====Returning Value From a Closure====")
+
+let drivingWithReturn = { (place: String) -> String in
+    return "Wohooo! I'm going to \(place) in my car!"
+}
+let message = drivingWithReturn("Palembang")
+print(message)
+
+//--------------------------------------------------------------------------------------------
 
 // closures as parameters
 // print("\n====Closures as Parameters====")
+
+
+//--------------------------------------------------------------------------------------------
 
 // trailing closure syntax
 // print("\n====Trailing Closure Syntax====")
