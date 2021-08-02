2ed4cf9fbed75d9fee96b39fc71ffa45f254f1f7
Andira Yunita <andhirayp@gmail.com>
2021-08-02 08:23:42 +0700

    starting closures

---
+++ HWS/Closures.swift
@@ -0,0 +1,18 @@
+print("============================================")
+print("\t\t\t\t  Closures")
+print("============================================")
+
+// creating basic closures
+// print("\n====Creating Basic Closures====")
+
+// accepting parameters in a closure
+// print("\n====Accepting Parameters in a Closure====")
+
+// returning value from a closure
+// print("\n====Returning Value From a Closure====")
+
+// closures as parameters
+// print("\n====Closures as Parameters====")
+
+// trailing closure syntax
+// print("\n====Trailing Closure Syntax====")
